da7e5e1e155efdf2d381e3740996516cf7de47d6
andrey <naumov.a.n@icloud.com>
2023-09-12 21:49:02 +0700

    Добавлен конструктр в фрилансер и перегрузка подсчёта оплаты

--- src/main/java/ru/gb/Freelancer.java
+++ src/main/java/ru/gb/Freelancer.java
@@ -1,4 +1,15 @@
 package ru.gb;
 
-public class Freelancer {
+public class Freelancer extends Employee{
+    private double hourlyRate; // ставка (чел/час)
+    protected Freelancer(String name, String surName, double hourlyRate) {
+        super(name, surName);
+        this.hourlyRate = hourlyRate;
+
+    }
+
+    @Override
+    public double calculateSalary() {
+        return 20.8 * 8 * hourlyRate;
+    }
 }
